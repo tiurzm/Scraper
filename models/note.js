32b0b9d84af7e0dedd6228c436c7f2197e398e53
Tiur Mawaddah <tiurzm@gmail.com>
2020-01-15 11:39:56 -0600

    create article, index and note.js on models

---
+++ models/note.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+    type: String,
+    required: true
+});
+
+const Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
